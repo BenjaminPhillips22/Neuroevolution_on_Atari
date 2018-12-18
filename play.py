f39d88c62930d6384f4275be2f94637b5855fc6f
Ben <benjamin.phillips22@gmail.com>
2018-12-18 21:12:24 +1100

    passed tests for reproducible random seed

--- play.py
+++ play.py
@@ -1,7 +1,14 @@
 import sys
 import cv2
 import time
+import random
+import numpy as np
 import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 import gym
 from gym import wrappers
@@ -149,21 +156,36 @@ def frostbite_simple_model(monitor=False, render=True):
     Try getting a simple model to run
     """
 
-    def reset(env):
-        return convert_state(env.reset())
-
-    def convert_state(state):
-        return cv2.resize(cv2.cvtColor(state, cv2.COLOR_RGB2GRAY), (64, 64)) / 255.0
-
     env = gym.make('Frostbite-v4')
     if monitor:
         env = wrappers.Monitor(env, 'v/ffrostbite-experiment-' + str(time.time()))
 
-    # simple model....
+    class Model(nn.Module):
+        """
+        A simple model....
+        """
+        def __init__(self, rng_state):
+            super().__init__()
 
-    
-    # observation = env.reset()
-    cur_states = [reset(env)] * 4
+            # TODO: padding?
+            self.rng_state = rng_state
+            torch.manual_seed(rng_state)
+
+            self.conv1 = nn.Conv2d(4, 16, (8, 8), 4)
+            self.conv2 = nn.Conv2d(16, 32, (4, 4), 2)
+            self.dense = nn.Linear(1152, 64)
+            self.out = nn.Linear(64, 18)
+
+        def forward(self, x):
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x))
+            x = x.view(1, -1)
+            x = F.relu(self.dense(x))
+            return self.out(x)
+
+    model = Model(2)
+
+    cur_states = [utils.reset(env)] * 4
     total_reward = 0
     total_frames = 0
 
@@ -172,24 +194,44 @@ def frostbite_simple_model(monitor=False, render=True):
         total_frames += 4
 
         #  model output
-        # values = model(Variable(torch.Tensor([cur_states])))[0] 
-        # action = np.argmax(values.data.numpy()[:env.action_space.n])
-        # print(action)
-        
+        values = model(Variable(torch.Tensor([cur_states])))[0]
+        action = np.argmax(values.data.numpy()[:env.action_space.n])
+        print(action)
+        observation, reward, done, info = env.step(action)
+
         if render:
             env.render()
             time.sleep(0.05)
 
-        # action = env.action_space.sample()  # select action randomly
-        observation, reward, done, info = env.step(action)
-
         # update current state
         cur_states.pop(0)
-        new_frame = convert_state(observation)
+        new_frame = utils.convert_state(observation)
         cur_states.append(new_frame)
         total_reward += reward
 
     env.env.close()
+
+
+def test_reproducible_random_seed():
+    """
+    Run a few models with identical random seeds and
+    see if the reward and action sequence are the same.
+    """
+    env = gym.make('Frostbite-v4')
+
+    random_seeds = list(range(1, 6))
+
+    for i, j in zip(random_seeds, random_seeds):
+        model_1 = utils.SmallModel(i)
+        model_2 = utils.SmallModel(i)
+
+        total_reward_m1, all_actions_m1 = utils.evaluate_model(env, model_1)
+        total_reward_m2, all_actions_m2 = utils.evaluate_model(env, model_2)
+
+        assert (total_reward_m1 == total_reward_m2) and (all_actions_m1 == all_actions_m2)
+
+    env.env.close()
+    print('all good :D')
 
 
 if __name__ == "__main__":
